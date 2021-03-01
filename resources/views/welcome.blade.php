45286367b0e8de10d3e8ae30e5d2ba150ebfa5b1
meligera@ya.ru <meligera@ya.ru>
2021-03-01 21:46:17 +0300

    12

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,515 @@
+<!DOCTYPE html>
+<html lang="en-US" class="no-js">
+	<head>
+
+		<!-- ==============================================
+		Title and Meta Tags
+		=============================================== -->
+		<meta charset="utf-8">
+		<title>Елизавета Купцова</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<!-- ==============================================
+		Favicons
+		=============================================== -->
+		<link rel="shortcut icon" href="assets/images/favicon.ico">
+		<link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="assets/images/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="assets/images/apple-touch-icon-114x114.png">
+		
+		<!-- ==============================================
+		CSS
+		=============================================== -->    
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/css/flexslider.css">
+		<link rel="stylesheet" href="assets/css/font-awesome.css">
+		<link rel="stylesheet" href="assets/css/styles-1.css">
+		
+		<!-- ==============================================
+		Fonts
+		=============================================== -->
+		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,200,600,700' rel='stylesheet' type='text/css'>
+		
+		
+		<!-- ==============================================
+		JS
+		=============================================== -->
+			
+		<!--[if lt IE 9]>
+			<script src="assets/js/libs/respond.min.js"></script>
+		<![endif]-->
+		
+		<script type="text/javascript" src="assets/js/libs/modernizr.min.js"></script>
+		
+	</head>
+  
+	<body data-spy="scroll" data-target="#main-nav" data-offset="400">
+	
+		<!--=== PAGE PRELOADER ===-->
+		<div id="page-loader"><span class="page-loader-gif"></span></div>
+		
+		<!-- ==============================================
+		MAIN NAV
+		=============================================== -->
+		<div id="main-nav" class="navbar navbar-fixed-top">
+			<div class="container">
+			
+				<div class="navbar-header">
+				
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#site-nav">
+						<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+					</button>
+					
+					<!-- ======= LOGO ========-->
+					<a class="navbar-brand scrollto" href="#home"><img src="assets/images/logo2.png" alt="" /></a>
+					
+				</div>
+				
+				<div id="site-nav" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<a href="https://www.youtube.com/channel/UCapwgTbhgYD1cfvLVJhAHsA" class="scrollto"><i class="fa fa-youtube-play" aria-hidden="true"></i></i>Мой канал</a>
+						</li>
+						<li>
+							<a href="#about" class="scrollto"><i class="fa fa-graduation-cap"></i>Обо мне</a>
+						</li>
+						<li>
+							<a href="#portfolio" class="scrollto"><i class="fa fa-briefcase"></i>Мои работы</a>
+						</li>
+						<li>
+							<a href="#contact" class="scrollto"><i class="fa fa-envelope"></i>Контакты</a>
+						</li>
+					</ul>
+				</div><!--End navbar-collapse -->
+				
+			</div><!--End container -->
+			
+		</div><!--End main-nav -->
+		
+		<!-- ==============================================
+		HEADER
+		=============================================== -->
+		<header id="home">
+		
+			<div class="container">
+			
+				<div class="row">
+				
+					<div class="col-sm-10 col-sm-offset-1 text-center">
+					
+						<h1 class="scrollimation fade-down d1">Елизавета Купцова</h1>
+						<h2 class="scrollimation fade-down d3"><span class="main-color">А</span>ктриса, <span class="main-color">М</span>узыкант, <span class="main-color">М</span>ечтатель</h2>
+					
+					</div>
+				
+				</div>
+			
+				<div class="header-img scrollimation fade-up d1">
+					<img class="img-responsive" src="assets/images/liza-test.png" alt="" />
+				</div>
+			
+			</div>
+			
+		</header><!--End header -->
+
+		<div id="main-content">
+			
+			<div class="scroll-down">
+				<a href="#services" class="scrollto">Узнать больше</a>
+			</div>
+			
+				
+			<!-- ==============================================
+			ABOUT
+			=============================================== -->
+			<section id="about" class="big-padding">
+			
+				<div class="container">
+					
+					<h1 class="section-title">Обо мне<span class="main-color">1</span></h1>
+				
+					<div class="row profile">
+					
+						<div class="col-sm-10 col-sm-offset-1 text-center">
+						
+							<figure class="hover-wrapper scrollimation fade-right">
+								<img class="img-responsive img-circle profile-img" src="assets/images/liza-img.jpg" alt="">
+								<figcaption class="hover-text">Это Я!</figcaption>
+							</figure>
+							
+							<h2 class="scrollimation fade-left">Елизавета Купцова</h2>
+							<h3 class="scrollimation fade-left">Музыкант</h3>
+						
+							<p class="margin-top scrollimation fade-right">Меня зовут Лиза, мне 19 лет и я выпускница Гнесинской академии. Я обожаю создавать что-то новое своими руками. Я сшила не одну сотню игрушек и хотела бы поделится секретами мастерства со всеми!</p>
+							
+							<p class="lead margin-top scrollimation fade-left">Создание что-то нового для меня не просто работа, это моя страсть. </p>
+							<p class="scrollimation fade-left"><a class="btn btn-dc btn-main-color scrollto" href="#portfolio">Мои работы</a></p>
+							
+						</div>
+						
+					</div>
+					
+				</div>
+			
+			</section>
+			
+			<!-- ==============================================
+			PORTFOLIO
+			=============================================== -->
+			<section id="portfolio" class="big-padding" >
+			
+				<div class="container">
+				
+					<h1 class="section-title">Мои работы<span class="main-color">2</span></h1>
+					<p class="subtitle">Все это может сшить любой желающий!</p>
+					
+					<!--==== Portfolio Filters ====-->
+					
+					<div class="text-center scrollimation fade-up">
+						<div id="filter-works" class="btn-group">
+							<button data-filter="*" type="button" class="btn btn-default active">Все работы<span class="project-count"></span></button>
+							<button data-filter=".web" type="button" class="btn btn-default">Новый Год<span class="project-count"></span></button>
+							<button data-filter=".print" type="button" class="btn btn-default">Подарки<span class="project-count"></span></button>
+							<button data-filter=".video" type="button" class="btn btn-default">Украшения<span class="project-count"></span></button>
+						</div>
+					</div>
+					
+				</div>	
+				
+				<div class="container scrollimation fade-up">
+			
+					<div id="projects-container" class="row">
+					
+						<!-- ==============================================
+						SINGLE PROJECT ITEM
+						=============================================== -->
+						
+						<a class="project-item col-sm-6 col-md-4 col-lg-3 web" href="#?slider-project.html">
+							
+							<img class="img-responsive project-image" src="assets/images/projects/thumb-1.jpg"  alt="">
+							
+							<div class="hover-title">
+								<h2 class="project-title">Project with Slider</h2><!--Project Title -->
+								<p>Brochure design</p><!--Project Subtitle -->
+							</div>
+							
+						</a>
+						
+						<a class="project-item col-sm-6 col-md-4 col-lg-3 web" href="#?youtube-project.html">
+							
+							<img class="img-responsive project-image" src="assets/images/projects/thumb-2.jpg"  alt="">
+							
+							<div class="hover-title">
+								<h2 class="project-title">Video project</h2><!--Project Title -->
+								<p>Youtube video</p><!--Project Subtitle -->
+							</div>
+							
+						</a>
+						
+						<a class="project-item col-sm-6 col-md-4 col-lg-3 web" href="#?images-project.html">
+							
+							<img class="img-responsive project-image" src="assets/images/projects/thumb-3.jpg"  alt="">
+							
+							<div class="hover-title">
+								<h2 class="project-title">Fullwidth images</h2><!--Project Title -->
+								<p>Professional Web Design</p><!--Project Subtitle -->
+							</div>
+							
+						</a>
+						
+						<a class="project-item col-sm-6 col-md-4 col-lg-3 web" href="#?vimeo-project.html">
+							
+							<img class="img-responsive project-image" src="assets/images/projects/thumb-4.jpg"  alt="">
+							
+							<div class="hover-title">
+								<h2 class="project-title">Another Video project</h2><!--Project Title -->
+								<p>Vimeo video</p><!--Project Subtitle -->
+							</div>
+							
+						</a>
+						
+						<a class="project-item col-sm-6 col-md-4 col-lg-3 print" href="#?slider-project.html">
+							
+							<img class="img-responsive project-image" src="assets/images/projects/thumb-6.jpg"  alt="">
+							
+							<div class="hover-title">
+								<h2 class="project-title">Project with Slider</h2><!--Project Title -->
+								<p>Modern Business Card</p><!--Project Subtitle -->
+							</div>
+							
+						</a>
+						
+						<a class="project-item col-sm-6 col-md-4 col-lg-3 print" href="#?images-project.html">
+							
+							<img class="img-responsive project-image" src="assets/images/projects/thumb-7.jpg"  alt="">
+							
+							<div class="hover-title">
+								<h2 class="project-title">Fullwidth images</h2><!--Project Title -->
+								<p>Poster Design</p><!--Project Subtitle -->
+							</div>
+							
+						</a>
+						
+						<a class="project-item col-sm-6 col-md-4 col-lg-3 print" href="#?slider-project.html">
+							
+							<img class="img-responsive project-image" src="assets/images/projects/thumb-8.jpg"  alt="">
+							
+							<div class="hover-title">
+								<h2 class="project-title">Project with Slider</h2><!--Project Title -->
+								<p>Personal Business Card</p><!--Project Subtitle -->
+							</div>
+							
+						</a>
+						
+						<a class="project-item col-sm-6 col-md-4 col-lg-3 print" href="#?vimeo-project.html">
+							
+							<img class="img-responsive project-image" src="assets/images/projects/thumb-5.jpg"  alt="">
+							
+							<div class="hover-title">
+								<h2 class="project-title">Video project</h2><!--Project Title -->
+								<p>Vimeo video</p><!--Project Subtitle -->
+							</div>
+							
+						</a>
+						
+						<a class="project-item col-sm-6 col-md-4 col-lg-3 video" href="#?slider-project.html">
+							
+							<img class="img-responsive project-image" src="assets/images/projects/thumb-9.jpg"  alt="">
+							
+							<div class="hover-title">
+								<h2 class="project-title">Project with Slider</h2><!--Project Title -->
+								<p>Professional Brand Identity</p><!--Project Subtitle -->
+							</div>
+							
+						</a>
+						
+						<a class="project-item col-sm-6 col-md-4 col-lg-3 video" href="#?vimeo-project.html">
+							
+							<img class="img-responsive project-image" src="assets/images/projects/thumb-10.jpg"  alt="">
+							
+							<div class="hover-title">
+								<h2 class="project-title">Another Video project</h2><!--Project Title -->
+								<p>Vimeo video</p><!--Project Subtitle -->
+							</div>
+							
+						</a>
+						
+						<a class="project-item col-sm-6 col-md-4 col-lg-3 video" href="#?images-project.html">
+							
+							<img class="img-responsive project-image" src="assets/images/projects/thumb-11.jpg"  alt="">
+							
+							<div class="hover-title">
+								<h2 class="project-title">Fullwidth images</h2><!--Project Title -->
+								<p>Creative Business Card</p><!--Project Subtitle -->
+							</div>
+							
+						</a>
+						
+						<a class="project-item col-sm-6 col-md-4 col-lg-3 video" href="#?youtube-project.html">
+							
+							<img class="img-responsive project-image" src="assets/images/projects/thumb-12.jpg"  alt="">
+							
+							<div class="hover-title">
+								<h2 class="project-title">Video project</h2><!--Project Title -->
+								<p>Youtube video</p><!--Project Subtitle -->
+							</div>
+							
+						</a>
+						
+					</div>	
+					
+					<p class="lead text-center">Вам понравилось? Подпишитесь на мой канал!</p>
+					<p class="text-center"><a class="btn btn-dc btn-main-color scrollto" href="https://www.youtube.com/channel/UCapwgTbhgYD1cfvLVJhAHsA">Подписаться</a></p>
+					
+				</div>	
+				
+			</section>
+			
+			<!-- ==============================================
+			TWITTER
+			=============================================== -->
+			<section id="twitter-feeds" class="bg-main-color">
+			
+				<div class="container text-center scrollimation fade-right">
+				
+					<div class="row">
+					
+						<div class="col-sm-10 col-sm-offset-1">
+						
+							<h1>Последние видео</h1>
+							<p><i class="fa fa-youtube-play fa-4x" aria-hidden="true"></i></p>
+							
+							<div id="twitter-slider" class="flexslider" data-widget-id="386017398488186880" data-max-tweets="5"></div>
+					
+							<p><a href="https://www.youtube.com/channel/UCapwgTbhgYD1cfvLVJhAHsA" data-toggle="tooltip" data-placement="bottom" title="Не забудьте подписаться!" target="_blank">@eliza_makes</a></p>
+						
+						</div>
+						
+					</div>
+					
+				</div>
+			
+			</section>
+			
+				
+			<!-- ==============================================
+			CONTACT
+			=============================================== -->
+			<section id="contact" class="big-padding light-typo color-overlay">
+			
+				<div class="container">
+				
+					<h1 class="section-title">Контакты<span class="main-color">3</span></h1>
+					
+					<div class="row">
+					
+						<div class="col-sm-6 col-md-5 scrollimation fade-right">
+						
+							<p class="lead">Я всегда рада Вам ответить! <br/>Готова услышить любые предложения.</p>
+						
+							<p>
+								Купцова Лиза<br/>
+								Blumenstrauß 192-1<br/>
+								France, Nice 892-722<br/>
+								(999) 777 - 1468 <br/>
+							</p>
+						
+							<div class="social-links">
+								<a href="#"><i class="fa fa-fw fa-twitter"></i></a>
+								<a href="#"><i class="fa fa-fw fa-facebook-square"></i></a>
+								<a href="#"><i class="fa fa-fw fa-linkedin-square"></i></a>
+							</div>
+						
+							<p><a class="mail-styled" href="mailto:mail@example.com">eliza-makes@google.com</a></p>
+						
+						</div>
+					
+						<!--=== Contact Form ===-->
+
+						<form id="contact-form" class="col-sm-6 col-md-offset-1 scrollimation fade-left" action="contact.php" method="post" novalidate>
+							
+							<div class="form-group">
+								<div class="controls">
+									<input id="contact-name" name="contactName" class="form-control requiredField" type="text" data-error-empty="is required">
+									<label class="control-label" for="contact-name">Имя</label>
+								</div>
+							</div><!-- End name input -->
+							
+							<div class="form-group">
+								<div class="controls">
+									<input id="contact-mail" name="email" class="form-control requiredField" type="email" data-error-empty="is required" data-error-invalid="is invalid">
+									<label class="control-label" for="contact-mail">Почта</label>
+								</div>
+							</div><!-- End email input -->
+							
+							<div class="form-group">
+								<div class="controls">
+									<textarea id="contact-message" name="comments" class="form-control requiredField" rows="6" data-error-empty="is required"></textarea>
+									<label class="control-label" for="contact-message">Сообщение</label>
+								</div>
+							</div><!-- End textarea -->
+							
+							<button name="submit" type="submit" class="btn" data-error-message="Please fill the required fields" data-sending-message="Sending Message" data-ok-message="Your Message has been sent!">Send It</button>
+							<input type="hidden" name="submitted" id="submitted" value="true" />
+							
+						</form><!-- End contact-form -->
+						
+					</div>
+					
+				</div>
+			
+			</section>
+			
+			<!-- ==============================================
+			FOOTER
+			=============================================== -->	
+			
+			<footer id="main-footer">
+			
+				<div class="container">
+				
+					<div class="row">
+					
+						<div class="col-sm-2">
+						
+							<a class="logo-link scrollto" href="#home">
+								<img class="img-responsive" src="assets/images/logo-bw2.png" alt="" />
+							</a>
+							
+						</div>
+					
+						<div class="col-sm-8">
+						
+							<nav>
+							
+								<ul class="footer-nav">
+									<li><a class="scrollto" href="#home">Главная</a></li>
+									<li><a class="scrollto" href="https://www.youtube.com/channel/UCapwgTbhgYD1cfvLVJhAHsA">Мой канал</a></li>
+									<li><a class="scrollto" href="#portfolio">Обо мне</a></li>
+									<li><a class="scrollto" href="#contact">Контакты</a></li>
+								</ul>
+							
+							</nav>
+						
+						</div>
+					
+						<div class="col-sm-2 text-right social-links">
+						
+							<a href="#" data-toggle="tooltip" title="Follow me on Twitter"><i class="fa fa-fw fa-twitter"></i></a>
+							<a href="#" data-toggle="tooltip" title="Follow me on Facebook"><i class="fa fa-fw fa-facebook"></i></a>
+							<a href="#" data-toggle="tooltip" title="Follow me on LinkedIn"><i class="fa fa-fw fa-linkedin"></i></a>
+						
+						</div>
+						
+					</div>
+				
+				</div>
+				
+			</footer>
+			
+		</div>
+		
+		<div id="project-viewer" class="modal fade">
+			
+			<span id="close-project-viewer" data-dismiss="modal">&times;</span>
+			
+			<div class="container">
+		
+				<div id="project-viewer-content"></div>
+				
+				<div id="project-viewer-footer">
+			
+					<div class="project-nav">
+					
+						<span class="previous-project" data-toggle="tooltip" data-placement="bottom" title="Previous Project"><i class="fa fa-long-arrow-left"></i></span>
+						<span class="back-to-portfolio" data-toggle="tooltip" data-placement="bottom" title="Back to Portfolio" data-dismiss="modal"><i class="fa fa-th-large"></i></span>
+						<span class="next-project" data-toggle="tooltip" data-placement="bottom" title="Next Project"><i class="fa fa-long-arrow-right"></i></span>
+					
+					</div>
+				</div>
+				
+			</div>
+			
+		</div><!-- /.modal -->
+			
+		
+		<!-- ==============================================
+		SCRIPTS
+		=============================================== -->	
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="assets/js/libs/jquery-1.9.1.min.js">\x3C/script>')</script>
+		
+		<script src="assets/js/libs/bootstrap.min.js"></script>
+		<script src="assets/js/jquery.easing.1.3.min.js"></script>
+		<script src="assets/js/waypoints.min.js"></script>
+		<script src="assets/js/jquery.easypiechart.js"></script>
+		<script src="assets/js/twitterFetcher_min.js"></script>
+		<script src="assets/js/jquery.flexslider.min.js"></script>
+		<script src="assets/js/jquery.fitvids.js"></script>
+		<script src="assets/js/contact.js"></script>
+		<script src="assets/js/scripts.js"></script>
+		
+	</body>
+	
+</html>
